ec4fae7e5f33bb6841c5116e7bb5b34c556db6be
sagorahamed1 <141553618+sagorahamed1@users.noreply.github.com>
2024-01-11 17:34:35 +0600

    Update question_model.dart

--- lib/model/question_model.dart
+++ lib/model/question_model.dart
@@ -1,11 +1,11 @@
-class QuestionModel {
-  final String question;
-  final int correctAnswerIndex;
+  class QuestionModel {
+    final String question;
+    final int correctAnswerIndex;
 
-  final List<String> options;
+    final List<String> options;
 
-  QuestionModel(
-      {required this.question,
-      required this.options,
-      required this.correctAnswerIndex});
+    QuestionModel(
+        {required this.question,
+        required this.options,
+        required this.correctAnswerIndex});
 }
